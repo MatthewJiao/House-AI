1d0a92a381a30edb6f1d268a4fb3f026ef6cc23c
Matthew Jiao <jiaomatthew222@gmail.com>
2021-01-17 01:04:00 -0500

    live deployment configs, api connection points

--- Front End/routes/index.js
+++ Front End/routes/index.js
@@ -156,7 +156,7 @@ router.post('/send-medical',ensureAuthenticated, (req, res) => {
     
     var input = req.body.pdf.concat(req.body.separator.concat(req.body.currentStr))
     //console.log(input)
-    var urlID = "http://127.0.0.1:5000/predict/".concat(input)
+    var urlID = String(MicroserviceAPI).concat(input)
     //console.log(urlID)
     axios({
         method: "GET",
