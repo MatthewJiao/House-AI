8805c6459b5d122e814f9655b261129dee51e3e8
Adam Lam <adamwlam26@gmail.com>
2021-09-10 08:48:26 -0400

    metrics

--- Quinte/prediction/metrics.py
+++ Quinte/prediction/metrics.py
@@ -1,4 +1,4 @@
 #metrics to test
 #have to run on the overall dataset and see if the rules/model is actually working
 
-
+#run on 16 in the end, run on test first, have an option in app.py that takes in adjusted labs and male female already. 
