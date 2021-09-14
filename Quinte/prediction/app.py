f7de46e964797e24d646e87d948b4d1368cf54f9
Adam Lam <adamwlam26@gmail.com>
2021-09-14 01:28:58 -0400

    metrics start

--- Quinte/prediction/app.py
+++ Quinte/prediction/app.py
@@ -101,7 +101,7 @@ def single_work(input = None, frontend = None):               #monitors a single
         for row4 in range(rows):
             fe = preprocessing.insert_into_dict(new_str_list[row4], fe)
 
-        result1 = rules.master_rules(fe, 0)
+        result1 = rules.master_rules(fe, rows)
 
         if result1==True:
             return "Sepsis was detected or is imminent in the patient."
--- Quinte/prediction/metrics.py
+++ Quinte/prediction/metrics.py
@@ -3,6 +3,25 @@
 
 #run on 16 in the end, run on test first, have an option in app.py that takes in adjusted labs and male female already. 
 
+import csv
+import pandas
 
 def run_on_dataset():
-    pass
+    # with open('test_metrics.csv', 'r') as file:
+    #     reader = csv.reader(file)
+    #     for row in reader:
+    #         print(row)'
+    df1 = pandas.read_csv("test_metrics.csv")
+    df1 = df1.drop('Unnamed: 0', 1)
+
+    print(df1)
+    #separate at every 0 in new_date
+        #going through each row and finding all the 0s
+        #splitting df into that many df[0:rowNUM] df[rowNum, rowNum2]
+        #list of dfs and list for rownum
+    #store into dict of lists properly
+    #call master_rules, changing curr each time
+    #also store sepsis prediction values
+    #change hour before constant 
+    #compare outputs
+    #create metrics
