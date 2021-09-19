41c47d83783b22c0e58ea5ff29718552f6976c9a
Adam Lam <adamwlam26@gmail.com>
2021-09-19 18:16:45 -0400

    metrics

--- Quinte/prediction/metrics.py
+++ Quinte/prediction/metrics.py
@@ -4,17 +4,138 @@
 #run on 16 in the end, run on test first, have an option in app.py that takes in adjusted labs and male female already. 
 
 import csv
+import rules
 import pandas
+import copy
+
+pt_identifier = []
+time = [] 
+Bp1 = [] 
+Bp2 = []
+Pulse = [] 
+Temp = []
+Rr = [] 
+O2Sat = [] 
+Age_Number = [] 
+Gender = []
+Lactate_value = [] 
+Lactate_ref_range = []  
+WBC_value = [] 
+WBC_ref_range = []
+Male = []
+Female = []
+Lactate_adjusted = []
+WBC_adjusted = []
+
+main_dict = {}
+
+main_dict["pt_identifier"] = pt_identifier
+main_dict["time"] = time
+main_dict["bp1"] = Bp1
+main_dict["bp2"] = Bp2
+main_dict["pulse"] = Pulse
+main_dict["temp"] = Temp
+main_dict["rr"] = Rr
+main_dict["o2sat"] = O2Sat
+main_dict["age_number"] = Age_Number
+main_dict["gender"] = Gender
+main_dict["lactate_value"] = Lactate_value
+main_dict["lactate_ref_range"] = Lactate_ref_range
+main_dict["wbc_value"] = WBC_value
+main_dict["wbc_ref_range"] = WBC_ref_range
+main_dict["male"] = Male
+main_dict["female"] = Female
+main_dict["lactate_adjusted"] = Lactate_adjusted
+main_dict["wbc_adjusted"] = WBC_adjusted
 
 def run_on_dataset():
+    dict_to_send = copy.deepcopy(main_dict)
     # with open('test_metrics.csv', 'r') as file:
     #     reader = csv.reader(file)
     #     for row in reader:
     #         print(row)'
-    df1 = pandas.read_csv("test_metrics.csv")
-    df1 = df1.drop('Unnamed: 0', 1)
+    df1 = pandas.read_csv("16.csv")
+    df1 = df1.drop(columns=['Unnamed: 0', 'Most_responsible', 'pre_admit', 'post_admit', 'secondary'])
 
-    print(df1)
+    #print(df1)
+    print ("\n\n\\nn\n")
+    rows = len(df1)
+    i = 0
+    correct_bool = False
+    correct = []
+    count = 0
+    while i<rows:
+        new_date = df1.iloc[i][3]
+        if new_date == 0:
+            j = i+1
+            while j<rows:
+                search_next_date = df1.iloc[j][3]
+                if search_next_date == 0:
+                    break
+                j+=1
+            
+            df_new = df1[i:j]
+
+            pos_or_neg = df1.iloc[0][1] #1 if pos, #0 if neg
+            df_new = df_new.drop(columns=['positive_diag', 'negative_diag'])
+
+            dict_to_send["pt_identifier"] = df_new['pt_identifier'].to_list()
+            dict_to_send["time"] = df_new['new_date'].to_list()
+            dict_to_send["bp1"] =  df_new['BP1'].to_list()
+            dict_to_send["bp2"] =  df_new['BP2'].to_list()
+            dict_to_send["pulse"] =  df_new['Pulse'].to_list()
+            dict_to_send["temp"] =  df_new['Temp'].to_list()
+            dict_to_send["rr"] =  df_new['RR'].to_list()
+            dict_to_send["o2sat"] = df_new['O2Sat'].to_list()
+            dict_to_send["age_number"] =  df_new['Age_Number'].to_list()
+            dict_to_send["gender"] = []
+            dict_to_send["lactate_value"] = []
+            dict_to_send["lactate_ref_range"] = []
+            dict_to_send["wbc_value"] = []
+            dict_to_send["wbc_ref_range"] = []
+            dict_to_send["male"] =  df_new['Male'].to_list()
+            dict_to_send["female"] =  df_new['Female'].to_list()
+            dict_to_send["lactate_adjusted"] =  df_new['Lactate_adjusted'].to_list()
+            dict_to_send["wbc_adjusted"] =  df_new['WBC_adjusted'].to_list()
+
+            #print(dict_to_send)
+            num_rows = len(dict_to_send["time"])
+
+            correct_bool = False
+            
+            for idx in range(num_rows):
+                correct_bool = False
+                result = rules.master_rules(dict_to_send, idx)
+                if result == True:
+                    count+=1
+                if result == True and pos_or_neg == 0:
+                    correct_bool = False
+                    break
+                elif result == True and pos_or_neg == 1:
+                    correct_bool = True
+                    break
+                elif result == False and pos_or_neg == 0:
+                    correct_bool = True
+                    break
+                
+            if correct_bool == True:
+                correct.append(1)
+            else:
+                correct.append(0)
+
+
+            #print(df_new) #works
+
+        i+=1
+
+    print ("length: ", len(correct))
+    num_correct = 0
+    for val in correct:
+        if val == 1:
+            num_correct+=1
+    print("num_right: ", num_correct)
+    print("accuracy: ", float(num_correct/len(correct)))
+    print ("positive count: ", count)
     #separate at every 0 in new_date
         #going through each row and finding all the 0s
         #splitting df into that many df[0:rowNUM] df[rowNum, rowNum2]
--- Quinte/prediction/rules.py
+++ Quinte/prediction/rules.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import preprocessing
 import const
+import math
 # Dataframe = pd.DataFrame
 
 #whenever new data entry is inputted, this runs. takes the past hour of info and averages it to compare with pre-decided values
@@ -29,7 +30,7 @@ def master_rules(dict_of_lists, curr) -> bool:
         if times[idx] >= hour_earlier and times[idx] <= curr_time:
             index_hour.append(idx)
 
-    print("index_hour", index_hour)
+    # print("index_hour", index_hour)
 
     #auto return rules here
     if age_rule(dict_of_lists, curr) == True: 
@@ -54,10 +55,14 @@ def master_rules(dict_of_lists, curr) -> bool:
 
 
 def age_rule(input, curr) -> bool:
-    age = int(input["age_number"][curr])
-    if age<=const.MIN_AGE:
-        print("Too Young for Prediction!")
-        return True
+    age = input["age_number"][curr]
+
+    if math.isnan(age) == False:
+        if age<=const.MIN_AGE:
+            # print("Too Young for Prediction!")
+            return True
+        else:
+            return False
     else:
         return False
 
@@ -68,8 +73,8 @@ def lactate_rule(input, index_hour) -> int:
     for idx in index_hour:
         lactate_val = float(input["lactate_adjusted"][idx])
         if lactate_val > const.LACTATE_ADJUSTED_LIMIT:
-            print("Lactate Over")
-            print("Lactate value: ", lactate_val)
+            # print("Lactate Over")
+            # print("Lactate value: ", lactate_val)
             return 1
     
     return 0
@@ -79,20 +84,23 @@ def rr_rule(input, index_hour) -> int:
     rr_sum = 0
     zero_count = 0
     for idx in index_hour:
-        val = int(input["rr"][idx])
-        if val != 0:
+        val = input["rr"][idx]
+        if val != 0 and math.isnan(val)==False and val != "nan" :
+            val = int(val)
             rr_sum = rr_sum + val
         else:
             zero_count+=1
-
-    rr_average = float(rr_sum/(len(index_hour)-zero_count))
+    try:
+        rr_average = float(rr_sum/(len(index_hour)-zero_count))
+    except:
+        return 0
 
     if rr_average >= const.RR_LIMIT:
-        print("RR OVER")
-        print("rr_average: ", rr_average)
+        # print("RR OVER")
+        # print("rr_average: ", rr_average)
         return 1
     else:
-        print("rr_average: ", rr_average)
+        # print("rr_average: ", rr_average)
         return 0
 
 
@@ -101,20 +109,23 @@ def pulse_rule(input, index_hour) -> int:
     zero_count = 0
 
     for idx in index_hour:
-        val = int(input["pulse"][idx])
-        if val != 0:
+        val = input["pulse"][idx]
+        if val != 0 and math.isnan(val)==False and val != "nan" :
+            val = int(val)
             pulse_sum = pulse_sum + val
         else:
             zero_count+=1
-
-    pulse_average = float(pulse_sum/(len(index_hour)-zero_count))
+    try:
+        pulse_average = float(pulse_sum/(len(index_hour)-zero_count))
+    except:
+        return 0
 
     if pulse_average >= const.HR_LIMIT:
-        print("PULSE OVER")
-        print("pulse_average: ", pulse_average)
+        # print("PULSE OVER")
+        # print("pulse_average: ", pulse_average)
         return 1
     else:
-        print("pulse_average: ", pulse_average)
+        # print("pulse_average: ", pulse_average)
         return 0
 
 
@@ -123,20 +134,23 @@ def temp_rule(input, index_hour) -> int:
     zero_count = 0
 
     for idx in index_hour:
-        val = int(input["temp"][idx])
-        if val != 0:
+        val = input["temp"][idx]
+        if val != 0 and math.isnan(val)==False and val != "nan" :
+            val = int(val)
             temp_sum = temp_sum + val
         else:
             zero_count+=1
-
-    temp_average = float(temp_sum/(len(index_hour)-zero_count))
+    try:
+        temp_average = float(temp_sum/(len(index_hour)-zero_count))
+    except: 
+        return 0
 
     if temp_average >= const.TEMP_UPPER_LIMIT or temp_average <= const.TEMP_LOWER_LIMIT:
-        print("TEMP OVER/UNDER")
-        print("temp_average: ", temp_average)
+        # print("TEMP OVER/UNDER")
+        # print("temp_average: ", temp_average)
         return 1
     else:
-        print("temp_average: ", temp_average)
+        # print("temp_average: ", temp_average)
         return 0
 
 
@@ -145,20 +159,23 @@ def bp1_rule(input, index_hour) -> int:
     zero_count = 0
 
     for idx in index_hour:
-        val = int(input["bp1"][idx])
-        if val != 0:
+        val = input["bp1"][idx]
+        if val != 0 and math.isnan(val)==False and val != "nan" :
+            val = int(val)
             bp1_sum = bp1_sum + val
         else:
             zero_count+=1
-
-    bp1_average = float(bp1_sum/(len(index_hour)-zero_count))
+    try:
+        bp1_average = float(bp1_sum/(len(index_hour)-zero_count))
+    except:
+        return 0
 
     if bp1_average <= const.BP1_LIMIT:
-        print("BP1 UNDER")
-        print("bp1_average: ", bp1_average)
+        # print("BP1 UNDER")
+        # print("bp1_average: ", bp1_average)
         return 1
     else:
-        print("bp1_average: ", bp1_average)
+        # print("bp1_average: ", bp1_average)
         return 0
 
 
@@ -168,8 +185,8 @@ def wbc_rule(input, index_hour) -> int:
     for idx in index_hour:
         wbc_val = float(input["wbc_adjusted"][idx])
         if wbc_val > const.WBC_ADJUSTED_LIMIT:
-            print("WBC Over")
-            print("WBC value: ", wbc_val)
+            # print("WBC Over")
+            # print("WBC value: ", wbc_val)
             return 1
     
     return 0
